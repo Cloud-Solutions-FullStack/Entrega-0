f18a4c00d2a8c5fde9495bfc4566899aa6bc0031
Santiago Bobadilla Suarez <s.bobadilla@uniandes.edu.co>
2025-01-29 23:13:28 -0500

    Local test pass

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
+  preview: {
     port: 3000,
     strictPort: true,
   },
-  preview: {
-    host: "0.0.0.0",
+  server: {
     port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
   },
   publicDir: "public",
 });
